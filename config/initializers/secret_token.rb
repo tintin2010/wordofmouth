c16e2ed29e40522e92aa2a66498c622c8d3abd4b
tintin2010 <seinjuti@gmail.com>
2013-10-27 12:42:08 +0530

    initial cut of word of mouth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wordofmouth::Application.config.secret_key_base = '5b5834858a961bee0b72821ee7b62610f61e7ac17bd64f4e0b253f32402ad166b952831b9f82a9f8fb8b63884df4549dacb98ad5c740566eed5b246fa3550333'
